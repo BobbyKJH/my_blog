e27319381de8150a70224ac5c59a03b387a839b4
BobbyKJH <rlatnguscjsw@gmail.com>
2022-07-24 02:24:20 +0900

    Feat: Main Page add Link Card

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,4 +1,27 @@
+import { Link } from "react-router-dom";
+import { MainPageStyle } from "../style/mainPage/MainPageStyle";
+
 const MainPage = () => {
-  return <div>1</div>;
+  return (
+    <MainPageStyle>
+      <Link to="/intro">
+        <div className="nav-box">
+          <span>자기소개</span>
+        </div>
+      </Link>
+
+      <Link to="/project">
+        <div className="nav-box">
+          <span>프로젝트</span>
+        </div>
+      </Link>
+
+      <Link to="/plan">
+        <div className="nav-box">
+          <span>계획</span>
+        </div>
+      </Link>
+    </MainPageStyle>
+  );
 };
 export default MainPage;
